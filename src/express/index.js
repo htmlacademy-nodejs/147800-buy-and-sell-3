fd7ea0dbbda3d15f1e71c836ec293eaf947c1cde
Alexandr <alexandrsashin@gmail.com>
2020-06-30 02:41:31 +0300

    Added pug templates, changed routes content

--- src/express/index.js
+++ src/express/index.js
@@ -1,16 +1,25 @@
 "use strict";
 
+const path = require(`path`);
 const express = require(`express`);
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+app.use((req, res) => res.status(400).render(`errors/404`));
+app.use((err, req, res, next) => res.status(500).render(`errors/500`));
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.listen(DEFAULT_PORT);

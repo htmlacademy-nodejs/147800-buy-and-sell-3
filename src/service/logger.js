6000518a5fde0687bb74e992eb3d34b7940c7ae7
Alexandr <alexandrsashin@gmail.com>
2020-11-17 22:34:16 +0300

    Added path to logs

--- src/service/logger.js
+++ src/service/logger.js
@@ -2,11 +2,14 @@
 
 const pino = require(`pino`);
 
-const logger = pino({
-  name: `pino-and-express`,
-  level: process.env.LOG_LEVEL || `info`,
-  prettyPrint: true,
-});
+const logger = pino(
+  {
+    name: `pino-and-express`,
+    level: process.env.LOG_LEVEL || `info`,
+    prettyPrint: true,
+  },
+  `./src/service/logs`
+);
 
 module.exports = {
   logger,

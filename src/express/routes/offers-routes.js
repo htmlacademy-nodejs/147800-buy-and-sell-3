906603305286bba921e3e60283d5644acd5ad4c4
Alexandr <alexandrsashin@gmail.com>
2020-11-20 00:17:31 +0300

    Working with post form data

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -6,7 +6,20 @@ const offersRouter = new Router();
 
 const URL = `http://localhost:3000`;
 
-offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
+offersRouter.get(`/add`, async (req, res) => {
+  const { data: categories } = await axios.get(`${URL}/api/categories`);
+  res.render(`offers/new-ticket`, {
+    categories,
+  });
+});
+offersRouter.post(`/add`, async (req, res) => {
+  try {
+    await axios.post(`${URL}/api/offers`, req.body);
+    res.redirect(`/my`);
+  } catch (error) {
+    res.redirect(`back`);
+  }
+});
 offersRouter.get(`/category/:id`, (req, res) => res.render(`offers/category`));
 offersRouter.get(`/edit/:id`, async (req, res) => {
   const { data: offer } = await axios.get(`${URL}/api/offers/${req.params.id}`);

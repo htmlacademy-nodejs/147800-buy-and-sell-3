eb40d82229e245ebf934e8245807feb06a8338ed
Alexandr <alexandrsashin@gmail.com>
2020-11-19 17:11:54 +0300

    Changed data and templates for /my route

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,9 +1,15 @@
 "use strict";
 
 const { Router } = require(`express`);
+const axios = require(`axios`);
 const myRouter = new Router();
 
+const URL = `http://localhost:3000`;
+
 myRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
-myRouter.get(`/`, (req, res) => res.render(`my/my-tickets`));
+myRouter.get(`/`, async (req, res) => {
+  const { data } = await axios.get(`${URL}/api/offers`);
+  res.render(`my/my-tickets`, { offers: data });
+});
 
 module.exports = myRouter;

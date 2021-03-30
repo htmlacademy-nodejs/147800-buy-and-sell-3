aeabe96b41c689ff658327ade0e9468462804dda
Alexandr Sashin <alexandrsashin@gmail.com>
2021-03-30 23:49:07 +0300

    Saved catching 400 error

--- src/express/index.js
+++ src/express/index.js
@@ -9,6 +9,9 @@ const mainRoutes = require(`./routes/main-routes`);
 
 const PORT = 8080;
 const PUBLIC_DIR = `public`;
+const HttpCode = {
+  BAD_REQUEST: 400
+};
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -19,10 +22,11 @@ app.set(`view engine`, `pug`);
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
-app.use(`/400`, (req, res) => res.render(`errors/404`));
 app.use(`/500`, (req, res) => res.render(`errors/500`));
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => res.status(HttpCode.BAD_REQUEST).render(`errors/404`));
 
 app.listen(PORT, () => {
   console.info(chalk.green(`Frontend server starts on PORT ${PORT}`));

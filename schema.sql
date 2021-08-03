845eee8bee1c028fd98772533fa4d4c31b0f2bfd
Alexandr Sashin <alexandrsashin@gmail.com>
2021-08-04 01:26:27 +0300

    Changed sql-files and readme

--- schema.sql
+++ schema.sql
@@ -1,130 +1,34 @@
---
--- PostgreSQL database dump
---
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS offer_categories;
+DROP TABLE IF EXISTS offers;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS categories;
 
--- Dumped from database version 10.15 (Ubuntu 10.15-0ubuntu0.18.04.1)
--- Dumped by pg_dump version 10.15 (Ubuntu 10.15-0ubuntu0.18.04.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: categories; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.categories (
-    id integer NOT NULL,
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
     label character varying(50) NOT NULL,
     picture character varying(50),
     retina_picture character varying(50)
 );
 
-
-ALTER TABLE public.categories OWNER TO postgres;
-
---
--- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.categories_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.categories_id_seq OWNER TO postgres;
-
---
--- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.categories_id_seq OWNED BY public.categories.id;
-
-
---
--- Name: comments; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.comments (
-    id integer NOT NULL,
-    offer_id integer NOT NULL,
-    text text NOT NULL,
-    created_at timestamp(0) without time zone NOT NULL,
-    user_id integer NOT NULL
+CREATE TABLE types (
+    id SERIAL PRIMARY KEY,
+    label character varying(50) NOT NULL
 );
 
-
-ALTER TABLE public.comments OWNER TO postgres;
-
---
--- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.comments_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.comments_id_seq OWNER TO postgres;
-
---
--- Name: comments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
-
-
---
--- Name: offer_categories; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.offer_categories (
-    offer_id integer NOT NULL,
-    category_id integer NOT NULL
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(50) NOT NULL,
+    last_name character varying(50) NOT NULL,
+    email character varying(50) NOT NULL,
+    picture character varying(50) NOT NULL,
+    retina_picture character varying(50) NOT NULL,
+    password character varying(200)
 );
 
-
-ALTER TABLE public.offer_categories OWNER TO postgres;
-
---
--- Name: offers; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.offers (
-    id integer NOT NULL,
+CREATE TABLE offers (
+    id SERIAL PRIMARY KEY,
     description text NOT NULL,
     picture character varying(50),
     retina_picture character varying(50),
@@ -132,314 +36,36 @@ CREATE TABLE public.offers (
     sum integer NOT NULL,
     type_id smallint NOT NULL,
     created_at timestamp(0) without time zone NOT NULL,
-    user_id integer NOT NULL
+    user_id integer NOT NULL,
+    CONSTRAINT offers_fk FOREIGN KEY (user_id) REFERENCES users(id) 
+        ON UPDATE CASCADE 
+        ON DELETE RESTRICT,
+    CONSTRAINT offers_fk_1 FOREIGN KEY (type_id) REFERENCES types(id) 
+        ON UPDATE CASCADE 
+        ON DELETE RESTRICT
 );
 
-
-ALTER TABLE public.offers OWNER TO postgres;
-
---
--- Name: offers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.offers_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.offers_id_seq OWNER TO postgres;
-
---
--- Name: offers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.offers_id_seq OWNED BY public.offers.id;
-
-
---
--- Name: types; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.types (
-    id integer NOT NULL,
-    label character varying(50) NOT NULL
+CREATE TABLE offer_categories (
+    offer_id integer NOT NULL,
+    category_id integer NOT NULL,
+    CONSTRAINT offer_categories_category_id_fkey FOREIGN KEY (category_id) REFERENCES categories(id) 
+        ON UPDATE SET NULL 
+        ON DELETE RESTRICT,
+    CONSTRAINT offer_categories_offer_id_fkey FOREIGN KEY (offer_id) REFERENCES offers(id) 
+        ON UPDATE SET NULL 
+        ON DELETE CASCADE
 );
 
-
-ALTER TABLE public.types OWNER TO postgres;
-
---
--- Name: types_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.types_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.types_id_seq OWNER TO postgres;
-
---
--- Name: types_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.types_id_seq OWNED BY public.types.id;
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    first_name character varying(50) NOT NULL,
-    last_name character varying(50) NOT NULL,
-    email character varying(50) NOT NULL,
-    picture character varying(50) NOT NULL,
-    retina_picture character varying(50) NOT NULL
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    offer_id integer NOT NULL,
+    text text NOT NULL,
+    created_at timestamp(0) without time zone NOT NULL,
+    user_id integer NOT NULL,
+    CONSTRAINT comments_fk FOREIGN KEY (user_id) REFERENCES users(id) 
+        ON UPDATE CASCADE 
+        ON DELETE RESTRICT,
+    CONSTRAINT comments_fk_1 FOREIGN KEY (offer_id) REFERENCES offers(id) 
+        ON UPDATE CASCADE 
+        ON DELETE CASCADE
 );
-
-
-ALTER TABLE public.users OWNER TO postgres;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.users_id_seq OWNER TO postgres;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- Name: categories id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.categories ALTER COLUMN id SET DEFAULT nextval('public.categories_id_seq'::regclass);
-
-
---
--- Name: comments id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.comments_id_seq'::regclass);
-
-
---
--- Name: offers id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.offers ALTER COLUMN id SET DEFAULT nextval('public.offers_id_seq'::regclass);
-
-
---
--- Name: types id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.types ALTER COLUMN id SET DEFAULT nextval('public.types_id_seq'::regclass);
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.categories (id, label, picture, retina_picture) FROM stdin;
-\.
-
-
---
--- Data for Name: comments; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.comments (id, offer_id, text, created_at, user_id) FROM stdin;
-\.
-
-
---
--- Data for Name: offer_categories; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.offer_categories (offer_id, category_id) FROM stdin;
-\.
-
-
---
--- Data for Name: offers; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.offers (id, description, picture, retina_picture, title, sum, type_id, created_at, user_id) FROM stdin;
-\.
-
-
---
--- Data for Name: types; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.types (id, label) FROM stdin;
-\.
-
-\.
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.users (id, first_name, last_name, email, picture, retina_picture) FROM stdin;
-\.
-
-
---
--- Name: categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.categories_id_seq', 1, true);
-
-
---
--- Name: comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.comments_id_seq', 1, true);
-
-
---
--- Name: offers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.offers_id_seq', 1, true);
-
-
---
--- Name: types_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.types_id_seq', 1, true);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
-
-
---
--- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.categories
-    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
-
-
---
--- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.comments
-    ADD CONSTRAINT comments_pkey PRIMARY KEY (id);
-
-
---
--- Name: offers offers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.offers
-    ADD CONSTRAINT offers_pkey PRIMARY KEY (id);
-
-
---
--- Name: types types_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.types
-    ADD CONSTRAINT types_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pk PRIMARY KEY (id);
-
-
---
--- Name: comments comments_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.comments
-    ADD CONSTRAINT comments_fk FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE RESTRICT;
-
-
---
--- Name: comments comments_fk_1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.comments
-    ADD CONSTRAINT comments_fk_1 FOREIGN KEY (offer_id) REFERENCES public.offers(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: offer_categories offer_categories_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.offer_categories
-    ADD CONSTRAINT offer_categories_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.categories(id) ON UPDATE SET NULL ON DELETE RESTRICT;
-
-
---
--- Name: offer_categories offer_categories_offer_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.offer_categories
-    ADD CONSTRAINT offer_categories_offer_id_fkey FOREIGN KEY (offer_id) REFERENCES public.offers(id) ON UPDATE SET NULL ON DELETE CASCADE;
-
-
---
--- Name: offers offers_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.offers
-    ADD CONSTRAINT offers_fk FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE RESTRICT;
-
-
---
--- Name: offers offers_fk_1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.offers
-    ADD CONSTRAINT offers_fk_1 FOREIGN KEY (type_id) REFERENCES public.types(id) ON UPDATE CASCADE ON DELETE RESTRICT;
-
-
---
--- PostgreSQL database dump complete
---
